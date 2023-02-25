f797cdb9cb57b83f75feb3f1fe19d1574b648615
Michael <michaelsupercode@gmail.com>
2023-02-25 15:51:23 +0100

    initial set

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const cors = require("cors")
+
+const usersArray = [
+    { id: 1, name: "Wolfgang", username: "wolfy" },
+    { id: 2, name: "Anna", username: "anna" },
+	{ id: 2, name: "Guido", username: "eddie" },
+    { id: 3, name: "Peter", username: "peter" },
+]
+
+const PORT = 1716
+const app = express()
+
+app.use(cors()) 
+
+app.use(express.json()) 
+
+app.use((req, _, next) => {
+    console.log("new req –", req.method, req.url)
+    next()
+})
+app.get("/", (req, res) => {
+    res.send("<h6>..it works so entirely very well..:</h6>")
+})
+app.get("/users", (req, res) => {
+    res.json(usersArray)
+})
+
+app.post("/newUser",
+    (req, res) => {
+        const user = req.body
+        const lastId = usersArray[usersArray.length - 1] ? usersArray[usersArray.length - 1].id : 1
+        const userWithId = {...user, id: lastId + 1 } 
+
+        usersArray.push(userWithId)
+        res.json(usersArray)
+    }
+)
+
+app.listen(PORT, () => console.log("Server listening on port", PORT))
